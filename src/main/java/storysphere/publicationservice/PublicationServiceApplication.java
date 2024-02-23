390b6a50e58ec30fe0b1e91db70996fcd2aad1b2
svyat-lag <svyatpalibza@mail.ru>
2024-02-23 13:54:56 +0300

    chore: init commit

---
+++ src/main/java/storysphere/publicationservice/PublicationServiceApplication.java
@@ -0,0 +1,13 @@
+package storysphere.publicationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/storysphere/publicationservice/PublicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package storysphere.publicationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
