8af08a37a9a9ef697c9c228f5e20ab120c189dd6
svyat-lag <svyatpalibza@mail.ru>
2024-03-03 15:08:26 +0300

    feat: API changes
    Add request mapper to the ServicePackageController ("/publication-service/api/v0.0.1"). So that this class is responsible for this specific API version.
    Also change mapper for addPublication method from "/addPublication" to "/add-publication".

--- src/main/java/storysphere/publicationservice/controller/ServicePackageController.java
+++ src/main/java/storysphere/publicationservice/controller/ServicePackageController.java
@@ -2,20 +2,18 @@ package storysphere.publicationservice.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import storysphere.publicationservice.model.RedisServicePackage;
 import storysphere.publicationservice.service.RedisServicePackageService;
 
 @RestController
+@RequestMapping("/publication-service/api/v0.0.1")
 @AllArgsConstructor
 public class ServicePackageController {
 
     private final RedisServicePackageService servicePackageService;
 
-    @PostMapping("/addPublication")
+    @PostMapping("/add-publication")
     @ResponseStatus(HttpStatus.CREATED)
     public void addPublication(@RequestBody RedisServicePackage servicePackage) {
         servicePackageService.addPublication(servicePackage);
