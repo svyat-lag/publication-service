6133818dd955d2986b631195691cb9ab9eb944f0
svyat-lag <svyatpalibza@mail.ru>
2024-03-03 22:50:19 +0300

    build: change hardcode values to env variables
    Update application.yml to use environment variables for configuration

--- src/main/java/storysphere/publicationservice/config/SecurityConfig.java
+++ src/main/java/storysphere/publicationservice/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
